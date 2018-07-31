bd9006266ff6205edd4ab66a693680e35662ac5b
Xiao-Yang Bu <xiaoyang.bu@hpe.com>
2018-07-31 20:53:34 +0800

    Function complete

---
+++ FileCopyTool/FileCopyTool/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace FileCopyTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxFileList = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSourceFolder = new System.Windows.Forms.TextBox();
+            this.textBoxTargetFolder = new System.Windows.Forms.TextBox();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonCopy);
+            this.panel1.Controls.Add(this.textBoxTargetFolder);
+            this.panel1.Controls.Add(this.textBoxSourceFolder);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(5, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(672, 96);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBoxFileList);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(5, 101);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(672, 447);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBoxFileList
+            // 
+            this.textBoxFileList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxFileList.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFileList.Location = new System.Drawing.Point(0, 0);
+            this.textBoxFileList.Multiline = true;
+            this.textBoxFileList.Name = "textBoxFileList";
+            this.textBoxFileList.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxFileList.Size = new System.Drawing.Size(672, 447);
+            this.textBoxFileList.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(0, 20);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(104, 20);
+            this.label.TabIndex = 0;
+            this.label.Text = "Source Folder:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TargetFolder:";
+            // 
+            // textBoxSourceFolder
+            // 
+            this.textBoxSourceFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSourceFolder.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSourceFolder.Location = new System.Drawing.Point(122, 16);
+            this.textBoxSourceFolder.Name = "textBoxSourceFolder";
+            this.textBoxSourceFolder.Size = new System.Drawing.Size(414, 24);
+            this.textBoxSourceFolder.TabIndex = 2;
+            // 
+            // textBoxTargetFolder
+            // 
+            this.textBoxTargetFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTargetFolder.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTargetFolder.Location = new System.Drawing.Point(122, 52);
+            this.textBoxTargetFolder.Name = "textBoxTargetFolder";
+            this.textBoxTargetFolder.Size = new System.Drawing.Size(414, 24);
+            this.textBoxTargetFolder.TabIndex = 3;
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCopy.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonCopy.Location = new System.Drawing.Point(547, 16);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(125, 60);
+            this.buttonCopy.TabIndex = 4;
+            this.buttonCopy.Text = "Start Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 553);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Copy Tool";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBoxFileList;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.TextBox textBoxTargetFolder;
+        private System.Windows.Forms.TextBox textBoxSourceFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ FileCopyTool/FileCopyTool/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileCopyTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            //Check source folder and target folder
+            if (!Directory.Exists(this.textBoxSourceFolder.Text.Trim()))
+            {
+                MessageBox.Show("\"Source Folder\" does not exist.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.textBoxSourceFolder.Focus();
+                return;
+            }
+            if (!Directory.Exists(this.textBoxTargetFolder.Text.Trim()))
+            {
+                MessageBox.Show("\"Target Folder\" does not exist.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.textBoxTargetFolder.Focus();
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach(string line in this.textBoxFileList.Lines)
+            {
+                try
+                {
+                    sb.Append(line);
+                    string sourceFilePath = Path.Combine(this.textBoxSourceFolder.Text.Trim(), line);
+                    string targetFilePath = Path.Combine(this.textBoxTargetFolder.Text.Trim(), line);
+                    string targetFolder = Path.GetDirectoryName(targetFilePath);
+                    if (!Directory.Exists(targetFolder))
+                    {
+                        Directory.CreateDirectory(targetFolder);
+                    }
+                    File.Copy(sourceFilePath, targetFilePath, true);
+                    sb.Append(String.Format("    Complete"));
+                }
+                catch (Exception ex)
+                {
+                    sb.Append(String.Format("    Error:{0}", ex.Message));
+                }
+                finally 
+                {
+                    sb.Append("\r\n");
+                }
+            }
+            this.textBoxFileList.Text = sb.ToString();
+            MessageBox.Show("Copy complete.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
